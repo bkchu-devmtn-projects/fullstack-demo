d7d48902db43fbd1a05a6fa08dc1b2873824f86f
Brandon Chung <brandon.chung@devmounta.in>
2018-06-18 09:16:45 -0500

    Update productsCtrl.js

--- server/controllers/productsCtrl.js
+++ server/controllers/productsCtrl.js
@@ -1,8 +1,9 @@
 module.exports = {
   getProducts: (req, res) => {
-    let db = req.app.get('db');
-    db.get_products().then(products => {
-      return res.status(200).send(products);
+    const db = req.app.get('db');
+    const { id } = req.params;
+    db.get_product_by_id([id]).then(product => {
+      return res.status(200).send(product);
     });
   }
 };
